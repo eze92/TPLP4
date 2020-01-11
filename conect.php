cdd417632cf0e32d8913d41bcd0367fafa20533f
eze92 <ezequiel.ledesma026@gmail.com>
2020-01-11 11:11:14 -0300

    se modifica archico connet

--- conect.php
+++ conect.php
@@ -1,8 +1,16 @@
 <?php
 	
-	$conexion=mysql_connect("mysql16.000webhost.com","a6449874_eze","dragon3392")
-	or exit ("No se puede conectar al servidor");
+	/*$conexion=mysql_connect("mysql16.000webhost.com","a6449874_eze","dragon3392")
+	or exit ("No se puede conectar al servidor");*/
 
+  $conexion = new mysqli("localhost","root","");
+  if(mysqli_connect_errno())
+  {
+	printf("fallo la conexion");
+  }
+  else {
+   printf("estas conectado"); 
+  }
 
 	//$base_datos=mysql_select_db("a6449874_turismo",$conexion)
 	//or exit ("No se puede conectar con la base de datos");
