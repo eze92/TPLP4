aa802f7f6e57ec520340ea61fd8a36f9ff8f2306
eze92 <ezequiel.ledesma026@gmail.com>
2020-01-31 16:24:45 -0300

    se modifica funcion modificar

--- controladores/modificarFuncion.php
+++ controladores/modificarFuncion.php
@@ -6,22 +6,19 @@
 	function ModificarAnecdota($nro_trans, $usu, $anecd)
 	{
 		include 'conect.php';
-		$sentencia= "UPDATE agenda SET usuario='".$usu."', anecdota='".$anecd."' WHERE nro_trans='".$nro_trans."' ";
 
-		$conexion->query($sentencia) or die ("Error al actualizar los datos".mysqli_error($conexion));
-	}
-	if ($_FILES["file1"]["error"] > 0){
-		
-	} else 
-	{
 		$nom_archivo=$_FILES['file1']['name']; // Para conocer el nombre del archivo
 		$ruta = "../imagenes/" . $nom_archivo;  // La ruta del archivo contiene el nuevo nombre y el tipo de extension
 		$archivo = $_FILES['file1']['tmp_name']; //el arhivo a subir
 		$subir=move_uploaded_file($archivo, $ruta); //se sube el archivo
-	    include 'conect.php';
+
+		$sentencia= "UPDATE agenda SET usuario='".$usu."', anecdota='".$anecd."',imagen='$ruta' WHERE nro_trans='".$nro_trans."' ";
+		$conexion->query($sentencia) or die ("Error al actualizar los datos".mysqli_error($conexion));
 		
-		$sentencia_img="UPDATE agenda SET imagen='$ruta' WHERE nro_trans='".$_POST['nro_trans']."' ";
-		$conexion->query($sentencia_img) or die ("Error al actualizar datos".mysqli_error($conexion));
+
+
+		///$sentencia_img="UPDATE agenda SET imagen='$ruta' WHERE nro_trans='".$_POST['nro_trans']."' ";
+		//$conexion->query($sentencia_img) or die ("Error al actualizar datos".mysqli_error($conexion));
 		
 	}
 ?>
