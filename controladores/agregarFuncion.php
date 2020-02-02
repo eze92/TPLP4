d516dbac1fd2b868d1ee1c7f9b74a787481978bf
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2020-02-02 13:08:10 -0300

    se agrega validacion de tipo y tamaño de imagen a subir

--- controladores/agregarFuncion.php
+++ controladores/agregarFuncion.php
@@ -8,17 +8,30 @@
 		include 'conect.php';
 
 		$nom_archivo=$_FILES['imagen']['name']; // Para conocer el nombre del archivo
-		$ruta = "../imagenes/" . $nom_archivo;  // La ruta del archivo contiene el nuevo nombre y el tipo de extension
+		$tamanio_imagen= $_FILES['imagen']['size'];
+		$maximo = 2000000; 
+		$tipo_imagen=$_FILES['imagen']['type'];
+		$tipos = array("image/gif","image/jpeg","image/jpg","image/png");
+
+		if (in_array($tipo_imagen,$tipos) && $tamanio_imagen <= $maximo) {
+
 		$archivo = $_FILES['imagen']['tmp_name']; //el arhivo a subir
+	   	$ruta = "../imagenes/" . $nom_archivo;  // La ruta del archivo contiene el nuevo nombre y el tipo de extension
 		$subir=move_uploaded_file($archivo, $ruta); //se sube el archivo
 
 		$sentencia= "INSERT INTO agenda (usuario, anecdota,imagen) VALUES ('".$usu."', '".$anecd."','".$ruta."') ";
 		$conexion->query($sentencia) or die ("Error al ingresar los datos".mysqli_error($conexion));	
+
+		echo '<script>	alert("Anecdota ingresada correctamente!!"); window.location.href="../modelo/agenda.php";</script>';
+		}	
+		else{       
+           	echo '<script>	alert("La anecdota no pudo ser ingresa, verifique el tipo de archivo y el tamaño de la imagen!!"); window.location.href="../modelo/agenda.php";</script>';
+		}
+		
 	}
+
+
 ?>
 
-<script type="text/javascript">
-	alert("Anecdota ingresada correctamente!!");
-	window.location.href='../modelo/agenda.php';
-</script>
+
 
