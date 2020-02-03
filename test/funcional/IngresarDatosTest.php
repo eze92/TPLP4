fa8f8e691f38403d7dc464ad6be8f9a580e22f05
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2020-02-03 19:56:33 -0300

    se renombran test

--- test/funcional/IngresarDatosTest.php
+++ test/funcional/IngresarDatosTest.php
@@ -56,7 +56,7 @@ class ProbandoIngresarDatosTest extends TestCase
 		
 		$this->webDriver->switchTo()->alert()->dismiss();	
 		
-		$this->assertStringContainsString('Turismo', $this->webDriver->getTitle());
+		$this->assertStringContainsString('Ezequiel', $this->webDriver-getAttribute("nombre"));
 	
 		}
 		
